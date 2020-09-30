69c976194d14f0078b4acbe15ba65b2b4c700e33
Pei-Ming Wu <p408865@gmail.com>
2020-09-30 19:57:01 +0800

    refactor: clean up styles

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,20 @@
   import AnimeList from "./components/AnimeList.svelte";
 </script>
 
+<style>
+  main {
+    align-content: center;
+    box-sizing: border-box;
+    font-family: 'SF Pro Text', 'SF Pro Icons', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
+    hyphens: auto;
+    line-height: 1.65;
+    margin: 0 auto;
+    max-width: 1200px;
+    min-height: 100vh;
+    text-align: center;
+  }
+</style>
+
 <svelte:head>
   <title>Anime Calendar</title>
 </svelte:head>
