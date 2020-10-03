1df6abe5105510b768e9dd1054facd9c8963bb81
Pei-Ming Wu <p408865@gmail.com>
2020-10-03 18:03:21 +0800

    feat: show/hide the anime list by css display attribute

--- src/components/AnimeList.svelte
+++ src/components/AnimeList.svelte
@@ -13,19 +13,29 @@
 </script>
 
 <style>
+    .outer {
+        background-color: #e2e2e2;
+        display: none;
+    }
+    .outer.show {
+        display: block;
+    }
     .scrollable {
         width: 100%;
         overflow-x: scroll;
-        background-color: #e2e2e2;
     }
     .container {
         display: flex;
         flex-direction: row;
         float: left;
     }
+    .loading {
+        margin: 0;
+        padding: 1em;
+    }
 </style>
 
-{#if show}
+<div class="outer" class:show={show}>
     {#if $animeStore.loaded}
         <div class="scrollable">
             <div class="container">
@@ -35,6 +45,6 @@
             </div>
         </div>
     {:else}
-        <p>Loading ...</p>
+        <p class="loading">Loading ...</p>
     {/if}
-{/if}
+</div>
