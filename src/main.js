e7308518313ff438f221e7258fdefd3e9be854b8
Pei-Ming Wu <p408865@gmail.com>
2020-10-04 16:19:57 +0800

    feat: store/load config from url hash

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import App from './App.svelte';
+import {deserializeConfig, serializeConfig} from "./utils";
+import {configStore} from "./store";
 
 const app = new App({
   target: document.body,
@@ -6,5 +8,29 @@ const app = new App({
     name: 'world',
   },
 });
+
+let configLoaded = false;
+
+// when config from store changes
+configStore.subscribe(updatedConfig => {
+  console.log({updatedConfig})
+  if (!configLoaded) {
+    return;
+  }
+  // update URL hash after store value has been changed
+  const serializedConfig = serializeConfig(updatedConfig);
+  window.location.hash = "#" + serializedConfig;
+})
+
+// on main app start, parse state from URL hash
+const hash = window.location.hash;
+if (hash && hash.length > 1) {
+  const serializedConfig = hash.substr(1);
+  const config = deserializeConfig(serializedConfig);
+  configStore.set(config);
+  configLoaded = true;
+} else {
+  configLoaded = true;
+}
 
 export default app;
