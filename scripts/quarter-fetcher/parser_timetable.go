cddb9d3f19797820dcf7e401d1bd1d167f5bd1fd
Pei-Ming Wu <p408865@gmail.com>
2020-10-10 09:30:25 +0800

    refactor: split episode parser into small methods

--- scripts/quarter-fetcher/parser_timetable.go
+++ scripts/quarter-fetcher/parser_timetable.go
@@ -26,41 +26,14 @@ func (p *timetableParser) Parse() error {
 		}
 
 		var episode Episode
-
-		channel := tr.Find("td.ch").Text()
-		timetable := timetableByChannel[channel]
-		episode.Channel = channel
-
-		tr.Find("td.subtitle").Contents().EachWithBreak(func(i int, s *goquery.Selection) bool {
-			if goquery.NodeName(s) == "#text" {
-				episode.Subtitle = s.Text()
-				return false
-			}
-			return true
-		})
-
-		var year, mon, day, hh, mm, ss, carry int
-		var weekDay rune
-		fmt.Sscanf(tr.Find("td.start").Text(), "%d-%d-%d(%c) %d:%d:%d ", &year, &mon, &day, &weekDay, &hh, &mm, &ss)
-		if hh >= 24 {
-			hh = hh - 24
-			carry = 1
-		}
-		parsedTimeString := fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02d+00:00", year, mon, day, hh, mm, ss)
-		if startTime, err := time.Parse(time.RFC3339, parsedTimeString); err == nil {
-			episode.StartAt = startTime.Add(time.Duration(carry) * time.Hour * 24)
-		}
-
-		if min, err := strconv.Atoi(tr.Find("td.min").Text()); err == nil {
-			episode.Duration = time.Duration(min) * time.Minute
-		}
-
-		if v, err := strconv.Atoi(tr.Find("td.count").Text()); err == nil {
-			episode.Number = v
-		}
-
+		p.parseEpisodeChannel(tr, &episode)
+		timetable := timetableByChannel[episode.Channel]
+		p.parseEpisodeSubtitle(tr, &episode)
+		p.parseEpisodeStartAt(tr, &episode)
+		p.parseEpisodeDuration(tr, &episode)
+		p.parseEpisodeNumber(tr, &episode)
 		timetable = append(timetable, episode)
-		timetableByChannel[channel] = timetable
+		timetableByChannel[episode.Channel] = timetable
 	})
 
 	p.anime.Channels = make([]string, 0)
@@ -69,10 +42,51 @@ func (p *timetableParser) Parse() error {
 	}
 
 	sort.Strings(p.anime.Channels)
+
 	p.anime.Timetables = make([]Timetable, 0)
 	for _, channel := range p.anime.Channels {
 		p.anime.Timetables = append(p.anime.Timetables, timetableByChannel[channel])
 	}
-
 	return nil
+}
+
+func (p *timetableParser) parseEpisodeChannel(tr *goquery.Selection, episode *Episode) {
+	channel := tr.Find("td.ch").Text()
+	episode.Channel = channel
+}
+
+func (p *timetableParser) parseEpisodeSubtitle(tr *goquery.Selection, episode *Episode) *goquery.Selection {
+	return tr.Find("td.subtitle").Contents().EachWithBreak(func(i int, s *goquery.Selection) bool {
+		if goquery.NodeName(s) == "#text" {
+			episode.Subtitle = s.Text()
+			return false
+		}
+		return true
+	})
+}
+
+func (p *timetableParser) parseEpisodeStartAt(tr *goquery.Selection, episode *Episode) {
+	var year, mon, day, hh, mm, ss, carry int
+	var weekDay rune
+	fmt.Sscanf(tr.Find("td.start").Text(), "%d-%d-%d(%c) %d:%d:%d ", &year, &mon, &day, &weekDay, &hh, &mm, &ss)
+	if hh >= 24 {
+		hh = hh - 24
+		carry = 1
+	}
+	parsedTimeString := fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02d+00:00", year, mon, day, hh, mm, ss)
+	if startTime, err := time.Parse(time.RFC3339, parsedTimeString); err == nil {
+		episode.StartAt = startTime.Add(time.Duration(carry) * time.Hour * 24)
+	}
+}
+
+func (p *timetableParser) parseEpisodeDuration(tr *goquery.Selection, episode *Episode) {
+	if min, err := strconv.Atoi(tr.Find("td.min").Text()); err == nil {
+		episode.Duration = time.Duration(min) * time.Minute
+	}
+}
+
+func (p *timetableParser) parseEpisodeNumber(tr *goquery.Selection, episode *Episode) {
+	if v, err := strconv.Atoi(tr.Find("td.count").Text()); err == nil {
+		episode.Number = v
+	}
 }
